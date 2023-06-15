08a802c40bba5f2407d2124335a61590ce6d4552
Adarsh Gourab Mahalik <gourabmahalikadarsh@gmail.com>
2023-06-15 07:03:51 +0530

    Have Done Some Recent Changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tweepy
 import schedule
 import bbc_feeds
 import time
+import random
 from config import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET, BEARER_TOKEN
 
 client = tweepy.Client(consumer_key=CONSUMER_KEY,
@@ -11,9 +12,10 @@ client = tweepy.Client(consumer_key=CONSUMER_KEY,
 
 def tweet_news():
     stories = bbc_feeds.news().all(limit=1)
-    client.create_tweet(text=''.join([f"📰{story.title}📰\n{story.summary}\nDonate BTC: bc1q0v5a27rk7s8xq6wzv5zhkhau2swj88c8m4k6dl" for story in stories]))
+    client.create_tweet(text=''.join([f"📰{story.title}📰\n{story.summary}\n{random.choice(['Donate BTC: bc1q0v5a27rk7s8xq6wzv5zhkhau2swj88c8m4k6dl',story.link])}" for story in stories]))
 
-schedule.every().hour.do(tweet_news)
+
+schedule.every(30).minutes.do(tweet_news)
 
 print('BOT RUNNING: %s' % schedule.jobs)
 while True:
